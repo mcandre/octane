38c00cefd8e5f36a1e5254946661caef0f9c6521
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2020-03-06 16:15:18 -0600

    close #25

--- octane.go
+++ octane.go
@@ -10,25 +10,28 @@ import (
 // Version is semver.
 const Version = "0.0.2"
 
+// Transpose applies a MIDI offset to a key.
+func Transpose(key uint8, offset int) uint8 {
+	return uint8((int(key) + offset) % 128)
+}
+
 // RegisterTranspose configures MIDI hooks for streaming,
 // with optional note transposition.
 func RegisterTranspose(reader *mid.Reader, midiOut mid.Out, offset int) {
 	writer := mid.ConnectOut(midiOut)
 
 	reader.Msg.Channel.NoteOn = func(_ *mid.Position, channel uint8, key uint8, velocity uint8) {
-		keyTransposed := uint8(int(key) + offset)
 		writer.SetChannel(channel)
 
-		if err := writer.NoteOn(keyTransposed, velocity); err != nil {
+		if err := writer.NoteOn(Transpose(key, offset), velocity); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 		}
 	}
 
 	reader.Msg.Channel.NoteOff = func(_ *mid.Position, channel uint8, key uint8, velocity uint8) {
-		keyTransposed := uint8(int(key) + offset)
 		writer.SetChannel(channel)
 
-		if err := writer.NoteOffVelocity(keyTransposed, velocity); err != nil {
+		if err := writer.NoteOffVelocity(Transpose(key, offset), velocity); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 		}
 	}
