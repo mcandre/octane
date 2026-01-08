c93276e6ef761269eeb23868430f74012661e1a4
Andrew <githubmcandre.gumdrop164@passfwd.com>
2026-01-08 13:20:50 -0600

    enable experimental targets

--- magefile.go
+++ magefile.go
@@ -115,7 +115,7 @@ func Xgo() error {
 		"-image",
 		image,
 		"-targets",
-		"darwin/amd64,darwin/arm64,linux/amd64,linux/arm64,windows/amd64",
+		"darwin/amd64,darwin/arm64,linux/386,linux/amd64,linux/arm,linux/arm64,linux/loong64,linux/mips,linux/mips64,linux/mips64le,linux/mipsle,linux/ppc64,linux/ppc64le,linux/riscv64,linux/s390x,windows/amd64",
 		".",
 	)
 }
